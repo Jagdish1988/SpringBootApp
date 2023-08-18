e7cda2d4d094e4b05a2f715bc2723889c81a9ef9
Jagdish1988 <34127027+Jagdish1988@users.noreply.github.com>
2023-08-18 11:30:03 +0530

    Update SpringBootAppApplication.java
    update the controller massage after installing the maven

--- src/main/java/com/jp/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/com/jp/SpringBootApp/SpringBootAppApplication.java
@@ -9,6 +9,7 @@ public class SpringBootAppApplication {
 	public static void main(String[] args) {
 		System.out.println("modified again");
 		System.out.println("modified again");
+		System.out.println("modified again");
 		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
